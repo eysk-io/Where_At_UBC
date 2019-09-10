efc148584e54b150afb016e8e5d1c1f543a56daa
Eric Y Kim <erickim195@gmail.com>
2019-09-10 10:18:46 -0700

    deliverable 1

--- src/main/placeholder/Main.java
+++ src/main/placeholder/Main.java
@@ -1,7 +1,17 @@
 package placeholder;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[]args) {
+        helloWorld();
+        goodJob();
     }
+
+    public static void helloWorld() {
+        System.out.println("Hello World!");
+    }
+
+    public static void goodJob() {
+        System.out.println("Good Job!");
+    }
+
 }
